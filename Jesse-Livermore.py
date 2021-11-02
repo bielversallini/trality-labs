72dbf9053df46f3820b0b0023fbb247f6166605d
Gabriel Barbosa <gabriel@versallini.com.br>
2021-11-02 01:33:22 -0300

    Compute signals (macd, stoch, ema)

--- Jesse-Livermore.py
+++ Jesse-Livermore.py
@@ -7,6 +7,9 @@ LONG    = "LONG"
 SHORT   = "SHORT"
 NEUTRAL = "NEUTRAL"
 
+OVERBOUGHT = "OVERBOUGHT"
+OVERSOLD   = "OVERSOLD"
+
 ############################################################################################################################
 
 def initialize(state):
@@ -14,13 +17,58 @@ def initialize(state):
 
 ############################################################################################################################
 
-def compute_signals(state, symbol, data):
-    # TODO
-    return
+def resolve_macd(state, data):
+    indicator = data.macd(12, 26, 9).last
+    buy = indicator[0] >= indicator[1] 
+
+    if buy:
+        signal = LONG
+    else:
+        signal = SHORT
+        
+    state.signals[data.symbol]['macd'] = signal
+
+def resolve_stoch(state, data):
+    indicator = data.stoch(8,3,3).last
+    buy = indicator[0] >= indicator[1]
+
+    if buy and indicator[0] < 80:
+        signal = LONG
+    elif buy and indicator[0] >= 80:
+        signal = OVERBOUGHT
+    elif not buy and indicator[0] > 20:
+        signal = SHORT
+    elif not buy and indicator[0] <= 20:
+        signal = OVERSOLD
+
+    state.signals[data.symbol]['stoch'] = signal
+
+def resolve_ema(state, data):
+    fast_ema = data.ema(3).last
+    medium_ema = data.ema(8).last
+    slow_ema = data.ema(20).last
+
+    signal = NEUTRAL
+
+    if fast_ema > medium_ema and medium_ema > slow_ema:
+        signal = LONG
+    elif fast_ema < medium_ema and medium_ema < slow_ema:
+        signal = SHORT
+
+    state.signals[data.symbol]['ema'] = signal
+    
+############################################################################################################################
+
+def compute_signals(state, data):
+    resolve_macd(state, data)
+    resolve_stoch(state, data)
+    resolve_ema(state, data)
+    
+    log("The signals for {} are {}".format(data.symbol, state.signals[data.symbol]), 2)
 
 ############################################################################################################################
 
-def process_orders(state, symbol, data):
+def process_orders(state, data):
     # TODO
     return
 
@@ -29,10 +77,14 @@ def process_orders(state, symbol, data):
 def execute(state, dataMap):
     for symbol, data in dataMap.items(): 
         if data is None:
-            return       
-        compute_signals(state, symbol, data)
+            return   
+             
+        if state.signals.get(symbol) == None:
+            state.signals[symbol] = {} 
 
-        process_orders(state, symbol, data)
+        compute_signals(state, data)
+
+        process_orders(state, data)
 
 ################################################# INTERVALS ################################################################
 
